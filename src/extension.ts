13f6ba9dd6d151d9dd6dbdcf86e8ad4afdaeca6b
Hayden McAlister <mcaha814@student.otago.ac.nz>
2024-06-06 12:09:44 +1200

    Add a shared function for setting up the context and getting user preferences

--- src/extension.ts
+++ src/extension.ts
@@ -9,3 +9,24 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() { }
+
+// Setup the functionality to insert a comment.
+// 
+// Returns the comment syntax, the number of hyphens to insert, and the line number.
+function performSetup(editor: vscode.TextEditor): [string, number, number] {
+	let document = editor.document;
+	let selection = editor.selection;
+	let line = document.lineAt(selection.active.line);
+	let languageId = document.languageId;
+	let commentSyntax = getCommentSyntax(languageId);
+	if (!commentSyntax) {
+		vscode.window.showInformationMessage(`No comment syntax found for language: ${languageId}`);
+		commentSyntax = "";
+	}
+
+	const config = vscode.workspace.getConfiguration("horizontal-line-comment");
+	const horizontalLineLength = config.get<number>('horizontalLineLength', 3);
+
+	return [commentSyntax, horizontalLineLength, line.lineNumber];
+}
+
